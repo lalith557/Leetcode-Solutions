03001fcfea5bf461bca4bd9bbcfcd2f2ca578ad0
Gona Lalith <140647155+lalith557@users.noreply.github.com>
2026-07-09 21:07:30 +0530

    Time: 0 ms (100%), Space: 14.9 MB (83.69%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int mid = n / 2;
+        int summation = 0;
+        for( int i = 0 ; i < n ; i++ ){
+            summation += mat[i][i];
+            summation += mat[n-1-i][i];
+        }
+        if( n % 2 == 1 ){   
+            summation -= mat[mid][mid];
+        }
+        return summation;
+    }
+};

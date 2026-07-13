c9b90a34d28a6c944e1333df8ea840dcc4bc46ad
Gona Lalith <140647155+lalith557@users.noreply.github.com>
2026-07-13 15:59:56 +0530

    Time: 0 ms (100%), Space: 21.7 MB (70.93%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == n) {
+                n++;
+            } else {
+                return n;
+            }
+        }
+        return n;
+    }
+};

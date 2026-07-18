3132342b0e2bd6373f874ced4ed44fd6a2a3ed4c
Gona Lalith <gonalalith2005@gmail.com>
2026-07-18 11:47:27 +0530

    Time: 6 ms (92.34%), Space: 11.8 MB (54.21%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,13 +9,12 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        //bool found= false;
         ListNode* slow = head;
-        ListNode* fast=head;
-        while(fast!=nullptr && fast->next!= nullptr){
-            slow=slow->next;
-            fast=fast->next->next;
-            if(fast == slow){
+        ListNode* fast = head;
+        while(fast != NULL && fast->next != NULL) {
+            slow =  slow->next;
+            fast = fast->next->next;
+            if(slow==fast) {
                 return true;
             }
         }
